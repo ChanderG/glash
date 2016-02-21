df2dc337c891d61f7948d6f0826f4d52d7143f3f
ChanderG <chandergovind@gmail.com>
2016-02-21 11:46:36 +0530

    add design principles

---
+++ glash.go
@@ -0,0 +1,16 @@
+/*
+ * glash: go lightweight anonymous shell
+ * Design objectives:
+ * 1. Just basic functionality
+ * 2. Heavily customized for my use. Not configuration.
+ * 3. No history saved anywhere.
+ * 4. Meant to be carried around -- not installed
+ * 5. First go project. Have fun.
+ */
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("> WIP")
+}
